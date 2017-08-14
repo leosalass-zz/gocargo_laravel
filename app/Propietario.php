dbc3a029502573f749ca2662ed9d32170fc5f058
Leonardo Salas <leosalass@gmail.com>
2017-08-13 19:34:32 -0500

    updates: actualizada de los modelosa de propietarios y vehiculos

--- app/Propietario.php
+++ app/Propietario.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Propietario extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'propietqarios';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['cedula', 'nombres', 'apellidos', 'ciudad'];
 }
--- app/Vehiculo.php
+++ app/Vehiculo.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Vehiculo extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'vechiculos';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['placa', 'color', 'propietario'];
 }
